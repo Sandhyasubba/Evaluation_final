4a8ecaaf9d42077a3ff99911fa9d3bed6fc1ae34
Sandy <156066569+Sandhyasubba@users.noreply.github.com>
2024-02-06 14:34:45 +0530

    Update add.c
    addition, difference , multiplication and division of 2 numbers.

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, sum, diff, mul, div;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -10,8 +10,14 @@ int main() {
     scanf("%d", &num2);
     
     sum = num1 + num2;
+    diff = num2 - num1;
+    mul = num1 * num2;
+    div = num2 / num1;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The diff of %d and %d is %d", num1,num2,diff);
+    printf("The mul of %d and %d is %d",num1,num2,mul);
+    printf("The div of %d and %d is %d",num1,num2,diff);
     
     return 0;
 }
